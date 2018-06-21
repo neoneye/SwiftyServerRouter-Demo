15b6715f44d2e68e4ef6fea96a99af9083c4cbef
Simon Strandgaard <neoneye@gmail.com>
2018-06-21 23:44:42 +0100

    Imported newest version and added /docs endpoint

--- Sources/SwiftyServerRouter-Demo/main.swift
+++ Sources/SwiftyServerRouter-Demo/main.swift
@@ -5,6 +5,7 @@ import PerfectHTTPServer
 extension RouteBuilder {
 	func my_configure() {
 		get("/healthcheck", EP_GetHealthcheck.self)
+		get("/docs", EP_GetEndpointDocumentation.self)
 		scope("/v1") {
 			get   ("/debug", EP_Debug.self)
 			post  ("/debug", EP_Debug.self)
